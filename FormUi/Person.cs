b5685ea7ee12e6bade3808fe0ed25b49113feca0
sepankorn <47814115+sepankorn@users.noreply.github.com>
2021-01-26 19:27:18 +0700

    Add project files.

---
+++ FormUi/Person.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FormUi
+{
+    public class Person
+    {
+        public int id { get; set; }
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public string Emailaddress { get; set; }
+        public string Phonenumber { get; set; }
+
+    }
+}
